6ca735c6e35065a5e373a6b2d5b93d8feebd8f96
Saul Machuca <smachuca@csumb.edu>
2025-03-25 10:07:59 -0700

    Initial Commit

---
+++ WidgetApp/WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppApp.swift
+//  WidgetApp
+//
+//  Created by Saul Machuca on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
